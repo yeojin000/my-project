f5a6eb87142c4835ec9bfcfa47a56158d263b2a1
yeojiny0@gmail.com <yeojiny0@gmail.com>
2025-11-21 17:05:20 +0900

    setupProxy 적용

--- seoul-culture/src/setupProxy.js
+++ seoul-culture/src/setupProxy.js
@@ -1,15 +1,15 @@
-// src/setupProxy.js
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app) {
+module.exports = function(app) {
   app.use(
-    '/seoulapi',
+    '/api/seoul',
     createProxyMiddleware({
-      target: 'https://openapi.seoul.go.kr:8088',
+      target: 'http://openapi.seoul.go.kr:8088',
       changeOrigin: true,
-      secure: true,          // https 타깃 인증서 검증
-      logLevel: 'debug',     // 프록시 로그 자세히 보기 (터미널에 출력)
-      pathRewrite: { '^/seoulapi': '' },
+      pathRewrite: {
+        // '/api/seoul'을 만나면 '실제키값'으로 바꿔서 요청을 보냄
+        '^/api/seoul': '/57516a724b79656f37316e536d4677'
+      },
     })
   );
 };
